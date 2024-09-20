696dea181465cfffd0d0eb891a4cd4303863fd4b
Yuuki Nishiyama <yuukin@iis.u-tokyo.ac.jp>
2024-09-21 03:59:22 +0900

    Support aware-core 0.7

--- Example/com.awareframework.ios.sensor.gyroscope/ViewController.swift
+++ Example/com.awareframework.ios.sensor.gyroscope/ViewController.swift
@@ -16,11 +16,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-//        sensor = GyroscopeSensor.init(GyroscopeSensor.Config().apply{config in
-//            config.debug = true
-//            config.sensorObserver = Observer()
-//        })
-//        sensor?.start()
+        sensor = GyroscopeSensor.init(GyroscopeSensor.Config().apply{config in
+            config.debug = true
+            config.sensorObserver = Observer()
+        })
+        sensor?.start()
     }
 
     class Observer:GyroscopeObserver{
